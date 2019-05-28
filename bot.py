738c4890e5e3b4a5e25763388ddcd6b2c517d04f
Homsik <faa31.31@mail.ru>
2019-05-28 08:20:11 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,56 @@
-import os
 import telebot
+import datetime
+import traceback
 from telebot import types
 import misc
-import re
+import os
+
 
 bot = telebot.TeleBot(misc.token)
-reg = re.compile('[\W\D+]')
+global path
 
-@bot.messge_handler(commands=['/start'])
-def start(m):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(*[types.KeyboardButton(name) for name in os.listdir()])
-    msg = bot.send_message(m.chat.id, 'Привет.', reply_markup=keyboard)
-    bot.register_next_step_handler(msg, name)
+@bot.message_handler(content_types=['text'])
+def message(message):
+    global path
+    keyboard = types.ReplyKeyboardRemove()
+    print('[{}] {}: {}'.format(datetime.datetime.now(), message.from_user.username, message.text))
 
-def name(m):
-    pass
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет', reply_markup=keyboard)
+
+    if message.text.split()[0] == '/dir':
+        files = []
+
+        if len(message.text.split()) > 2:
+            keyboard_width = message.text.split()[2]
+        else:
+            keyboard_width = str(misc.keyboard_width)
+
+        if len(message.text.split()) == 1:
+            bot.send_message(message.chat.id, '/dir [path] [keyboard_width=misc.keyboard_width]', reply_markup=keyboard)
+        else:
+            path = message.text.split()[1]
+            files_buffer = os.listdir(path)
+            for file in files_buffer:
+                if len(file.split('.')) == 2: files.append(file)
+            keyboard = types.InlineKeyboardMarkup()
+
+            if keyboard_width == '1':
+                for file in files: keyboard.add(types.InlineKeyboardButton(text=file, callback_data=file))
+            if keyboard_width == '2':
+                for file in range(1, len(files) // 2 * 2, 2):
+                    button1 = types.InlineKeyboardButton(text=files[file - 1], callback_data=path + '/' + files[file - 1])
+                    button2 = types.InlineKeyboardButton(text=files[file], callback_data=path + '/' + files[file])
+                    keyboard.add(button1, button2)
+                if len(files) / 2 - len(files) // 2 != 0:
+                    keyboard.add(types.InlineKeyboardButton(text=files[-1], callback_data=path + '/' + files[-1]))
+
+            bot.send_message(message.chat.id, 'Директория: {}\nНайдено файлов: {}'.format(path, len(files)), reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def call_back_room(call):
+    print('Start {}... '.format(call.data), end='')
+    os.system(call.data)
 
 bot.polling()
